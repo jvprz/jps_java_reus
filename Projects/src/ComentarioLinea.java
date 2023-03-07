16a901ca2622b5c2b17f2404754fe1f62ee1ff1e
Javier Prz <58307741+jvprz@users.noreply.github.com>
2023-03-07 13:51:10 +0100

    Delete Projects directory

--- Projects/src/ComentarioLinea.java
+++
@@ -1,8 +0,0 @@
-
-public class ComentarioLinea {
-	public static void main(String args[]) {
-		//System.out.println("Hola");
-		System.out.println("Àdios");
-		System.out.println("FIN DEL PROGRAMA");
-	}
-}
