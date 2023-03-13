44adb5306398d76a5a93f69cabd178ba3729baad
JPrimee <javierpsantos99@gmail.com>
2023-03-13 10:30:05 +0100

    Github desktop test

---
+++ UD04/src/Ejercicio2.java
@@ -0,0 +1,13 @@
+
+public class Ejercicio2 {
+	public static void main(String args[]) {
+		int n = 5;
+		double a = 4.56;
+		char c = 'a';
+		
+		System.out.println(n + " " + a + " " + c);	//Todos los valores
+		System.out.println(n+a); //Suma de n + a
+		System.out.println(a-n); //Diferencia a-n
+		System.out.println((int) c); //Valor del caracter a
+	}
+}
