2499cd2a72983c1c0ded73d824f1fbd259ce0dd7
JPrimee <javierpsantos99@gmail.com>
2023-04-07 20:16:58 +0200

    4

---
+++ UD20/com/src/main/java/Ejercicio_4/Ejercicio_4.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package Ejercicio_4;
+
+/**
+ * @author javier
+ *
+ */
+public class Ejercicio_4 {
+
+}
---
+++ UD20/com/src/main/java/Ejercicio_4/Ejercicio_4_MainApp.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package Ejercicio_4;
+
+import javax.swing.*;
+
+/**
+ * @author javie
+ *
+ */
+public class Ejercicio_4_MainApp extends JFrame{
+
+}
